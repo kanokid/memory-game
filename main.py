7dbeaab9c987f34df05d3292a063c19da2b50bf6
kanokid <57643139+kanokid@users.noreply.github.com>
2025-12-11 20:14:23 -0500

    (WIP) worked on matching and game win logic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cmu_graphics import *
 import random
-from time import sleep
+app.stepsPerSecond = 1
 possibleSymbols = ['snowman', 'snowflake', 'present', 'wreath']
 app.symbols = possibleSymbols + possibleSymbols
 random.shuffle(app.symbols)
@@ -24,82 +24,100 @@ app.firstCardUp = False
 app.bothCardsUp = False
 app.firstCardIdentifier = 0
 app.secondCardIdentifier = 0
+app.matchCheckTimer = 0
+app.matchedCards = []
 def onMousePress(mouseX,mouseY):
     if mouseX < 125 and mouseX >= 0 and mouseY < 200:
-        card1.fill = None
-        if app.firstCardUp == True:
-            app.bothCardsUp = True
-            app.firstCardUp = False
-            app.secondCardIdentifier = 1
-        else:
-            app.firstCardUp = True
-            app.firstCardIdentifier = 1
+        if 1 not in app.matchedCards:
+            card1.fill = None
+            if app.firstCardUp == True:
+                app.bothCardsUp = True
+                app.firstCardUp = False
+                app.secondCardIdentifier = 1
+            else:
+                app.firstCardUp = True
+                app.firstCardIdentifier = 1
     elif mouseX < 250 and mouseX > 125 and mouseY < 200:
-        card2.fill = None
-        if app.firstCardUp == True:
-            app.bothCardsUp = True
-            app.firstCardUp = False
-            app.secondCardIdentifier = 2
-        else:
-            app.firstCardUp = True
-            app.firstCardIdentifier = 2
+        if 2 not in app.matchedCards:
+            card2.fill = None
+            if app.firstCardUp == True:
+                app.bothCardsUp = True
+                app.firstCardUp = False
+                app.secondCardIdentifier = 2
+            else:
+                app.firstCardUp = True
+                app.firstCardIdentifier = 2
     elif mouseX < 375 and mouseX > 250 and mouseY < 200:
-        card3.fill = None
-        if app.firstCardUp == True:
-            app.bothCardsUp = True
-            app.firstCardUp = False
-            app.secondCardIdentifier = 3
-        else:
-            app.firstCardUp = True
-            app.firstCardIdentifier = 3
+        if 3 not in app.matchedCards:
+            card3.fill = None
+            if app.firstCardUp == True:
+                app.bothCardsUp = True
+                app.firstCardUp = False
+                app.secondCardIdentifier = 3
+            else:
+                app.firstCardUp = True
+                app.firstCardIdentifier = 3
     elif mouseX < 500 and mouseX > 375 and mouseY < 200:
-        card4.fill = None
-        if app.firstCardUp == True:
-            app.bothCardsUp = True
-            app.firstCardUp = False
-            app.secondCardIdentifier = 4
-        else:
-            app.firstCardUp = True
-            app.firstCardIdentifier = 4
+        if 4 not in app.matchedCards:
+            card4.fill = None
+            if app.firstCardUp == True:
+                app.bothCardsUp = True
+                app.firstCardUp = False
+                app.secondCardIdentifier = 4
+            else:
+                app.firstCardUp = True
+                app.firstCardIdentifier = 4
     elif mouseX < 125 and mouseX > 0 and mouseY > 200:
-        card5.fill = None
-        if app.firstCardUp == True:
-            app.bothCardsUp = True
-            app.firstCardUp = False
-            app.secondCardIdentifier = 5
-        else:
-            app.firstCardUp = True
-            app.firstCardIdentifier = 5
+        if 5 not in app.matchedCards:
+            card5.fill = None
+            if app.firstCardUp == True:
+                app.bothCardsUp = True
+                app.firstCardUp = False
+                app.secondCardIdentifier = 5
+            else:
+                app.firstCardUp = True
+                app.firstCardIdentifier = 5
     elif mouseX < 250 and mouseX > 125 and mouseY > 200:
-        card6.fill = None
-        if app.firstCardUp == True:
-            app.bothCardsUp = True
-            app.firstCardUp = False
-            app.secondCardIdentifier = 6
-        else:
-            app.firstCardUp = True
-            app.firstCardIdentifier = 6
+        if 6 not in app.matchedCards:
+            card6.fill = None
+            if app.firstCardUp == True:
+                app.bothCardsUp = True
+                app.firstCardUp = False
+                app.secondCardIdentifier = 6
+            else:
+                app.firstCardUp = True
+                app.firstCardIdentifier = 6
     elif mouseX < 375 and mouseX > 250 and mouseY > 200:
-        card7.fill = None
-        if app.firstCardUp == True:
-            app.bothCardsUp = True
-            app.firstCardUp = False
-            app.secondCardIdentifier = 7
-        else:
-            app.firstCardUp = True
-            app.firstCardIdentifier = 7
+        if 7 not in app.matchedCards:
+            card7.fill = None
+            if app.firstCardUp == True:
+                app.bothCardsUp = True
+                app.firstCardUp = False
+                app.secondCardIdentifier = 7
+            else:
+                app.firstCardUp = True
+                app.firstCardIdentifier = 7
     elif mouseX < 500 and mouseX > 375 and mouseY > 200:
-        card8.fill = None
-        if app.firstCardUp == True:
-            app.bothCardsUp = True
-            app.firstCardUp = False
-            app.secondCardIdentifier = 8
-        else:
-            app.firstCardUp = True
-            app.firstCardIdentifier = 8
+        if 8 not in app.matchedCards:
+            card8.fill = None
+            if app.firstCardUp == True:
+                app.bothCardsUp = True
+                app.firstCardUp = False
+                app.secondCardIdentifier = 8
+            else:
+                app.firstCardUp = True
+                app.firstCardIdentifier = 8
+    if app.bothCardsUp == True:
+        app.matchCheckTimer = 2
+        print ("hi")
 def checkIfMatch():
-    if app.firstCardIdentifier == app.secondCardIdentifier and app.firstCardIdentifier != 0 and app.secondCardIdentifier != 0:
-        pass
+    if app.symbols[app.firstCardIdentifier-1]  == app.symbols[app.secondCardIdentifier-1] and app.firstCardIdentifier != 0 and app.secondCardIdentifier != 0:
+        app.matchedCards.append(app.firstCardIdentifier)
+        app.matchedCards.append(app.secondCardIdentifier)
+        if len(app.matchedCards) == 8:
+            print("You win!")
+    else:
+        resetNonMatchedCards()
 def resetAllCards():
     card1.fill = "white"
     card2.fill = "white"
@@ -113,4 +131,10 @@ def resetAllCards():
     app.bothCardsUp = False
     app.firstCardIdentifier = 0
     app.secondCardIdentifier = 0
+def onStep():
+    if app.matchCheckTimer > 0:
+        app.matchCheckTimer -= 1
+        if app.matchCheckTimer == 0:
+            checkIfMatch()
+            app.bothCardsUp = False
 cmu_graphics.run()
